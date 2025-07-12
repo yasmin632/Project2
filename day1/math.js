82fe9f8c43dc7ef8f61059c800398c32a116b9d8
yasmin632 <yasminsanadi777@gmail.com>
2025-07-12 15:55:26 +0530

    Add files via upload

---
+++ day1/math.js
@@ -0,0 +1,12 @@
+export const add=(a,b)=>
+{
+    return a+b;
+}
+  export const sub=()=>
+  {
+    return a-b;
+  }
+    //function name()
+    //{
+    //}
+    //name();
---
+++ day1/script.js
@@ -0,0 +1,33 @@
+console.log("hello");
+const name="js";
+var name1="java";
+let name2="script";
+
+
+
+
+ function greet(){
+const name1="java";
+return name1;
+ }
+ // console.log(greet());
+ console.log(greet());
+
+ for(let i=10;i>0;i--)
+ {
+    console.log(i);
+
+ }
+ 
+console.log("__________"); 
+ 
+
+
+
+
+ let arr=[10,20,30,40];
+ arr.push(50);
+ console.log(arr);
+ console.log(arr.length);
+
+
